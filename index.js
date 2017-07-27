cd692c78591c4686a68bd1b96fba3a4c13b3679d
shapan1 <shapandashore@gmail.com>
2017-07-27 19:31:48 -0400

    FB authentication try#1

--- index.js
+++ index.js
@@ -19,11 +19,21 @@ app.get('/', function (req, res) {
 });
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'Darth_Vadar_is_my_father') {
-		res.send(req.query['hub.challenge']);
-	}
-	res.send('Error, wrong token');
+// app.get('/webhook/', function (req, res) {
+// 	if (req.query['hub.verify_token'] === ) {
+// 		res.send(req.query['hub.challenge']);
+// 	}
+// 	res.send('Error, wrong token');
+// });
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'Darth_Vadar_is_my_father') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
 });
 
 // Spin up the server
