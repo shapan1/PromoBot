7dff2c91b09127258439713578288eeab034ecc2
shapan1 <shapandashore@gmail.com>
2017-07-27 20:50:38 -0400

    added generic message sender

--- index.js
+++ index.js
@@ -68,7 +68,56 @@ function sendTextMessage(sender, text) {
 
 //const token = "EAAbbRkzb8RYBAMMpnwvr8ZBQjT20RzTa2EUTeZAE4OslqLpeqJsYoE6f4SQ0NGQBygwyohAYSXdOVsWU5QJSzsd90keLycQsMWCCC33ZBujOwAyKYkTh6U5obw7IoSUGDGPeTj0CQ6WzZC1QBIqJwu02JnRaPq95M0uWZCSwdqwZDZD";
 
+//text cards
 
+function sendGenericMessage(sender) {
+    let messageData = {
+	    "attachment": {
+		    "type": "template",
+		    "payload": {
+				"template_type": "generic",
+			    "elements": [{
+					"title": "First card",
+				    "subtitle": "Element #1 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+				    "buttons": [{
+					    "type": "web_url",
+					    "url": "https://www.messenger.com",
+					    "title": "web url"
+				    }, {
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for first element in a generic bubble",
+				    }],
+			    }, {
+				    "title": "Second card",
+				    "subtitle": "Element #2 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+				    "buttons": [{
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for second element in a generic bubble",
+				    }],
+			    }]
+		    }
+	    }
+    };
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+	    json: {
+		    recipient: {id:sender},
+		    message: messageData,
+	    }
+    }, function(error, response, body) {
+	    if (error) {
+		    console.log('Error sending messages: ', error);
+	    } else if (response.body.error) {
+		    console.log('Error: ', response.body.error);
+	    }
+    });
+};
 
 // Spin up the server
 app.listen(app.get('port'), function() {
